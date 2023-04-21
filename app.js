4c31e799046599d6872e4cc6602f2e23ff470242
RocioMartinez <rym5411@gmail.com>
2023-04-21 03:10:49 -0400

    getting into functions

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ console. log("You made it to my console!");
 const userName = prompt("What should we call you?");
 console. log (userName);
 
+function getName(){
+    const userName= prompt('What is your name');
+    return userName;
+}
+
+function userMessage(userName){
+    if (userName == ''){
+        userName = prompt('Try again');
+    }if (userName == 'rocio'){
+        alert('My master!');
+    } else if ( userName == 'kassie'){
+        alert('Uh-oh');
+    } else {
+        document. write ('stranger');
+    }
+}
+
 
 document. write ('Hi ' + userName + ', lets be friends!');
 
@@ -23,6 +40,10 @@ confirm ('Ready to look at potatoes?');
 if (confirm= true){
     document. write('Lets do this!');
 } else {
-    document. write('But I love potatoes')
+    document. write('I love potatoes')
 }
 
+function getName(){
+    const userName= prompt('What is your name');
+    return userName;
+}
