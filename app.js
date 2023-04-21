2535ce3c6883f1c0ef208a8ab92cf20534191e1f
RocioMartinez <rym5411@gmail.com>
2023-04-21 09:38:57 -0400

    finalizing 7,8

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 'use strict';
 
-console. log("You made it to my console!");
+console.log("You made it to my console!");
 
 
-const userName = prompt("What should we call you?");
-console. log (userName);
+// const userName = prompt("What should we call you?");
+// console. log (userName);
 
-function getName(){
-    const userName= prompt('What is your name?');
+
+function getName() {
+    const userName = prompt('What is your name?');
+    console.log(userName);
     return userName;
 }
 
-getName();
+let theirName = getName();
 
-function userMessage(userName){
-    if (userName == ''){
-        userName = prompt('Try again');
-    }if (userName == 'rocio'){
+
+function userMessage() {
+    while (theirName == '') {
+        theirName = prompt('Try again');
+    }
+
+    if (theirName == 'rocio') {
         alert('My master!');
-    } else if ( userName == 'kassie'){
+    } else if (theirName == 'kassie') {
         alert('Uh-oh');
     } else {
-        console. log (' stranger');
+        console.log(' stranger');
     }
 }
 
 userMessage();
 
 
-document. write ('Hi ' + userName + ', lets be friends!');
+document.write('Hi ' + theirName + ', lets be friends!');
 
 // if (userName == 'rocio'){
 //     alert('My master!');
@@ -39,16 +44,27 @@ document. write ('Hi ' + userName + ', lets be friends!');
 // }
 
 
-   function likePotato(){
-     confirm ('Ready to look at potatoes?');}
-     let y= 1;
-     let x= 0;
-     let theirAnswer= (y, x);
-        if (theirAnswer= 1){
-          document. write ('Lets do this!');
-         } 
-        else if (0);{
-          document. write ('Only potato lovers allowed!');
-        }
- 
- likePotato();
+//   
+// function likePotato(){
+//     confirm ('Ready to look at potatoes?');}
+//     let y= 1;
+//     let x= 0;
+//     let theirAnswer= (y, x);
+//        if (theirAnswer= 1){
+//          document. write ('Lets do this!');
+//         } 
+//        else if (0);{
+//          document. write ('Only potato lovers allowed!');
+//     }
+
+
+function likePotato() {
+    let x = confirm('Ready to look at potatoes?');
+    console.log(x);
+    if (x == true) {
+        document.write('Lets do this!');
+    } else {
+        document.write('Only potato lovers allowed!');
+    }
+}
+
